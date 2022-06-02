6df846fd317913f1e85453ec61fb23cb66766930
unknown <vishal170997@gmail.com>
2022-06-02 16:26:32 +0530

    updated structure

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, flash, redirect, jsonify
+import config, csv, datetime
+from binance.client import Client
+from binance.enums import *
+
+app = Flask(__name__)
+app.secret_key = b'somelongrandomstring'
+
+client = Client(config.API_KEY_BINANCE, config.API_SECRET_BINANCE)
+
+@app.route('/')
+def index():
+    title = 'CoinView'
+
+    account = client.get_account()
+
+    balances = account['balances']
+
+    exchange_info = client.get_exchange_info()
+    symbols = exchange_info['symbols']
+
+    return render_template('index.html', title=title, my_balances=balances, symbols=symbols)
